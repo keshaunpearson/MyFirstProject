c5e6352c42ca34e0e708488c03a70e518ae4398d
kdpea <kdpea@LAPTOP-88I8BSEP>
2021-07-22 01:02:35 -0500

    Add project files.

---
+++ MyFirstProject/Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MessageLabel = New System.Windows.Forms.Label()
+        Me.EnglishButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.PrintButton = New System.Windows.Forms.Button()
+        Me.SpanishButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.SuspendLayout()
+        '
+        'MessageLabel
+        '
+        Me.MessageLabel.AutoSize = True
+        Me.MessageLabel.Location = New System.Drawing.Point(415, 167)
+        Me.MessageLabel.Name = "MessageLabel"
+        Me.MessageLabel.Size = New System.Drawing.Size(0, 25)
+        Me.MessageLabel.TabIndex = 0
+        '
+        'EnglishButton
+        '
+        Me.EnglishButton.Location = New System.Drawing.Point(136, 394)
+        Me.EnglishButton.Name = "EnglishButton"
+        Me.EnglishButton.Size = New System.Drawing.Size(195, 74)
+        Me.EnglishButton.TabIndex = 1
+        Me.EnglishButton.Text = "English"
+        Me.EnglishButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(536, 548)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(195, 69)
+        Me.ExitButton.TabIndex = 2
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'PrintButton
+        '
+        Me.PrintButton.Location = New System.Drawing.Point(536, 394)
+        Me.PrintButton.Name = "PrintButton"
+        Me.PrintButton.Size = New System.Drawing.Size(195, 74)
+        Me.PrintButton.TabIndex = 3
+        Me.PrintButton.Text = "Print"
+        Me.PrintButton.UseVisualStyleBackColor = True
+        '
+        'SpanishButton
+        '
+        Me.SpanishButton.Location = New System.Drawing.Point(136, 548)
+        Me.SpanishButton.Name = "SpanishButton"
+        Me.SpanishButton.Size = New System.Drawing.Size(195, 74)
+        Me.SpanishButton.TabIndex = 4
+        Me.SpanishButton.Text = "Spanish"
+        Me.SpanishButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(657, 684)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(172, 25)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "by KeShaun Pearson"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(912, 821)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.SpanishButton)
+        Me.Controls.Add(Me.PrintButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.EnglishButton)
+        Me.Controls.Add(Me.MessageLabel)
+        Me.Name = "Form1"
+        Me.Text = "Hello World by KeShaun Pearson"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MessageLabel As Label
+    Friend WithEvents EnglishButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents PrintButton As Button
+    Friend WithEvents SpanishButton As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+End Class
---
+++ MyFirstProject/Form1.vb
@@ -0,0 +1,39 @@
+﻿'Project:       Hello World (Form1.vb / MyFirstProject.sln)
+'Programmer:    KeShaun Pearson
+'Date:          7/21/2021
+'Description:   This project will display a "Hello World" message in two different languages and print the form
+
+
+Public Class Form1
+
+    Public Shared Sub Main()
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        'Exit the project
+
+        Me.Close()
+    End Sub
+
+    Private Sub EnglishButton_Click(sender As Object, e As EventArgs) Handles EnglishButton.Click
+        'Display the Hello World Message // comment 
+
+        MessageLabel.Text = "Hello World"
+    End Sub
+
+    Private Sub SpanishButton_Click(sender As Object, e As EventArgs) Handles SpanishButton.Click
+        'Display the Hello World Message in Spanish // comment 
+
+        MessageLabel.Text = "Hola Mundo"
+    End Sub
+
+    Private Sub PrintButton_Click(sender As Object, e As EventArgs) Handles PrintButton.Click
+        'Print the form
+
+        'PrintForm PowerPack Unavailable 
+
+
+
+
+    End Sub
+End Class
